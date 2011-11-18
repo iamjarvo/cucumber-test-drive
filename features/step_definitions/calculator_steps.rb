17c248210d63e35c97796466cccf9bab454ba696
Jearvon Dharrie <j.dharrie@gmail.com>
2011-11-18 14:37:47 -0500

    added prints 4 to make add test pass

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -1,11 +1,24 @@
-Given /^the input "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+require 'open3'
+
+# this is for the last step "then the output should be"
+require 'test/unit'
+include Test::Unit::Assertions
+
+Given /^the input "([^"]*)"$/ do |input|
+  File.open('input.txt', 'w') do |file|
+    file.puts(input)
+  end
 end
 
 When /^the calculator is run$/ do
-  pending # express the regexp above with the code you wish you had
+  command = "ruby ../calculator.rb input.txt"
+  Open3.popen3(command) do |stdin, stdout, stderr|
+    error_message = stderr.read
+    raise(error_message) unless error_message.empty?
+    @output = stdout.read
+  end
 end
 
-Then /^the output should be "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^the output should be "([^"]*)"$/ do |expected_output|
+  assert_equal(expected_output, @output, "The out was not what was expected")
 end
