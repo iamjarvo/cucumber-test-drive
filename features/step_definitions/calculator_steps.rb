0350ce995df6421a2ad5ded47d0a7a0652dedfc4
Jearvon Dharrie <j.dharrie@gmail.com>
2011-11-18 14:45:17 -0500

    test passes. had the wrong file path for calculator.rb in the test

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -11,7 +11,7 @@ Given /^the input "([^"]*)"$/ do |input|
 end
 
 When /^the calculator is run$/ do
-  command = "ruby ../calculator.rb input.txt"
+  command = "ruby calculator.rb input.txt"
   Open3.popen3(command) do |stdin, stdout, stderr|
     error_message = stderr.read
     raise(error_message) unless error_message.empty?
@@ -20,5 +20,5 @@ When /^the calculator is run$/ do
 end
 
 Then /^the output should be "([^"]*)"$/ do |expected_output|
-  assert_equal(expected_output, @output, "The out was not what was expected")
+  assert_equal(expected_output, @output, "The output was not what was expected")
 end
