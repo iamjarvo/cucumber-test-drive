c24cdc500792058a3b2b7eb87be0394fa99a92d6
Jearvon Dharrie <j.dharrie@gmail.com>
2011-11-18 15:08:31 -0500

    implemented the addition feature.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
-print "4"
+input = File.read(ARGV[0]) # read the commandline argument
+numbers_to_add = input.split('+').map { |n| n.to_i } # split the contents of the file based on the  + sign
+total = 0
+numbers_to_add.each do |number| # iterate of the numbers
+  total += number # add them to the total
+end
+
+print total # print toatal
